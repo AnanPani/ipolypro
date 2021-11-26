6c4b79a6ad3fbc3ac9c6584ba27ee98b2bbb946f
Anan Panichakarn <ananpanichakarn@Anans-MacBook-Pro.local>
2021-11-26 08:54:09 +0700

    Fix Layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'iPolyPro';
 
+  title = 'iPolyPro';
+  isIn = true;
+  left = true;
+  _Isshow = false;
+
+
+
+  get right() {
+    return !this.left;
+  }
+
+  get isOut() {
+    return !this.isIn;
+  }
+
+  togglesidenav(item : boolean) {
+    if (item === false){
+      this._Isshow = true;
+    }
+    else {
+      this._Isshow = false;
+    }
+  }
 }
